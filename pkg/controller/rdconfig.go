2ac00b09ca39ee862b09557580455bb13541a56f
anmol372 <anmrasto@cisco.com>
2021-09-28 09:20:25 -0700

    logs for debugging

--- pkg/controller/rdconfig.go
+++ pkg/controller/rdconfig.go
@@ -18,14 +18,21 @@ package controller
 
 import (
 	"context"
+	"os"
+	"reflect"
+	"runtime"
+
 	rdConfigv1 "github.com/noironetworks/aci-containers/pkg/rdconfig/apis/aci.snat/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"os"
-	"reflect"
 )
 
 func (cont *AciController) syncRdConfig() bool {
+	_, file, no, ok := runtime.Caller(1)
+	if ok {
+		cont.log.Debugf("called from %s#%d\n", file, no)
+	}
+	cont.log.Debug("Syncing RdConfig")
 	var options metav1.GetOptions
 	var discoveredSubnets []string
 	cont.indexMutex.Lock()
@@ -35,13 +42,16 @@ func (cont *AciController) syncRdConfig() bool {
 	cont.indexMutex.Unlock()
 	env := cont.env.(*K8sEnvironment)
 	rdConfigClient := env.rdConfigClient
+	cont.log.Debugf("RdConfig Client: %#v", rdConfigClient)
 	if rdConfigClient == nil {
 		return false
 	}
 	ns := os.Getenv("ACI_SNAT_NAMESPACE")
 	name := os.Getenv("ACI_RDCONFIG_NAME")
+	cont.log.Debugf("RdConfig name: %s, namespace: %s", name, ns)
 	rdConfig, err := rdConfigClient.AciV1().RdConfigs(ns).Get(context.TODO(), name, options)
 	if err != nil {
+		cont.log.Debugf("Unable to get RDConfig: %s, err: %v", name, err)
 		if apierrors.IsNotFound(err) {
 			rdConfigInstance := &rdConfigv1.RdConfig{
 				ObjectMeta: metav1.ObjectMeta{
@@ -54,15 +64,18 @@ func (cont *AciController) syncRdConfig() bool {
 			}
 			_, err = rdConfigClient.AciV1().RdConfigs(ns).Create(context.TODO(), rdConfigInstance, metav1.CreateOptions{})
 			if err != nil {
+				cont.log.Debugf("Unable to create RDConfig: %s, err: %v", name, err)
 				return true
 			}
-			cont.log.Debug("RdConfig Created: ", rdConfigInstance, err)
+			cont.log.Debugf("RdConfig Created: ", rdConfigInstance, err)
 		}
 	} else {
+		cont.log.Debugf("Comparing existing rdconfig DiscoveredSubnets with cached values")
 		if !reflect.DeepEqual(rdConfig.Spec.DiscoveredSubnets, discoveredSubnets) {
 			rdConfig.Spec.DiscoveredSubnets = discoveredSubnets
 			_, err = rdConfigClient.AciV1().RdConfigs(ns).Update(context.TODO(), rdConfig, metav1.UpdateOptions{})
 			if err != nil {
+				cont.log.Debugf("Unable to Update RDConfig: %s, err: %v", name, err)
 				return true
 			}
 			cont.log.Debug("RdConfig Updated: ", discoveredSubnets)
